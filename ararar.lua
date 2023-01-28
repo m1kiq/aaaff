9c127d846d3d4144538f8260e68cf1d56cdfa212
m1kiq <123826580+m1kiq@users.noreply.github.com>
2023-01-28 18:30:23 +0200

    Create ararar.lua

---
+++ ararar.lua
@@ -0,0 +1,8 @@
+local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/Robojini/Tuturial_UI_Library/main/UI_Template_1"))()
+local Window = Library.CreateLib("name", "RJTheme1")
+local Tab = Window:NewTab("qwe")
+local Section = Tab:NewSection("bot")
+
+Section:NewSlider("SliderText", "SliderInfo", 500, 0, function(s) -- 500 (Макс. значение) | 0 (Мин. значение)
+    game:GetService("Workspace").fodfas.Humanoid.WalkSpeed = s	
+and)
